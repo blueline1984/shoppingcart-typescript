929a136417b21d2eeee9a5ee6e9e98f322c94a3f
been <blueline1318@gmail.com>
2022-09-03 17:09:46 +0900

    [FEAT]: Add Routes & navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Item from "./pages/Item";
+import Discount from "./pages/Discount";
+import ShoppingCart from "./pages/ShoppingCart";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1>Shopping Cart</h1>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Item />} />
+        <Route path="/discount" element={<Discount />} />
+        <Route path="/shoppingcart" element={<ShoppingCart />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
